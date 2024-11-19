027c1488899aedf29978141c2c5e59160e10ce06
mgdabrowska <malgorzatadabrowska13@wp.pl>
2024-11-19 11:22:37 +0100

    change to task 9

--- koty-webapp/src/main/java/pl/kobietydokodu/koty/controllers/KotyController.java
+++ koty-webapp/src/main/java/pl/kobietydokodu/koty/controllers/KotyController.java
@@ -1,3 +1,5 @@
+package main.java.pl.kobietydokodu.koty.controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
